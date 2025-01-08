a80b02af1371ac2255779df6cf5a1c82562e7845
0xwebmilk <0xwebmilk@gmail.com>
2025-01-08 21:27:06 +0300

    Some changes

--- interactors/user_interactor.py
+++
@@ -1,6 +0,0 @@
-class UserInteractor:
-    def __init__(self, repository):
-        self.repository = repository
-
-    def create(self, name, email, password):
-        return self.repository.create(name, email, password)
