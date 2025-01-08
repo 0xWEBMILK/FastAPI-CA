62cabbe680d2b6f3dcf5a51fe1ce5f03cfcd3edc
0xwebmilk <0xwebmilk@gmail.com>
2025-01-08 21:27:22 +0300

    Some changes

--- configs/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-
-def get_db_connection():
-    engine = create_engine("postgresql+psycopg2://postgres:Ddz180905@localhost/postgres", echo=True)
-    session = sessionmaker(bind=engine)
-
-    return session
