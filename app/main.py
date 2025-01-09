75f0d7549b3e7c9d4cb11ec1d6762236f0be4f7e
0xwebmilk <0xwebmilk@gmail.com>
2025-01-09 11:23:02 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from app.configs.database import get_session_stub, database_metadata_init
 def main():
     app = FastAPI()
 
-    engine = create_engine("wo-wo-wo-wo-wo-wo", echo=True)
+    engine = create_engine("what?-hard-connection-in-repo?-yes-cus-its-a-test-repo-lil-smart-boy", echo=True)
     session = sessionmaker(bind=engine)
     database_metadata_init(engine)
 
