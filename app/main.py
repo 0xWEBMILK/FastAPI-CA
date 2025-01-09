790b84fe194dea1b7da9f5d7ba1f59c6a8b2201b
0xwebmilk <0xwebmilk@gmail.com>
2025-01-09 05:08:50 +0300

    Some changes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 import uvicorn
 
 from app.controllers.user_controller import user_router
-from app.configs.database import get_session_stub, init
+from app.configs.database import get_session_stub, database_metadata_init
 
 
 def main():
@@ -13,12 +13,26 @@ def main():
 
     engine = create_engine("postgresql+psycopg2://postgres:Ddz180905@localhost/postgres", echo=True)
     session = sessionmaker(bind=engine)
-    init(engine)
+    database_metadata_init(engine)
+
 
     def new_session():
+        # This function creates a new session using a context manager, ensuring proper resource management and transaction handling.
+        # - The 'with' statement ensures that the session is automatically closed when the context exits.
+        # - The function yields the session to allow operations within its scope.
+        # - If operations succeed, the changes are committed using s.commit().
+        # - If an exception occurs, the changes are rolled back using s.rollback(), maintaining data integrity.
+        # - The session is guaranteed to close in the 'finally' block, even in case of errors, preventing resource leaks.
         with session() as s:
-            yield s
-            s.commit()
+            try:
+                yield s
+                s.commit()
+            except Exception as e:
+                s.rollback()
+                raise e
+            finally:
+                s.close()
+
 
     app.dependency_overrides[get_session_stub] = new_session
 
