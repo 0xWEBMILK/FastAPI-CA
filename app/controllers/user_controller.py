229c0b43bd3746b065bf9aa9272d9bcc52eb5a6c
0xwebmilk <0xwebmilk@gmail.com>
2025-01-09 05:08:02 +0300

    Added CRUD
    Added crud methods
    Added type hints

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -6,11 +6,38 @@ from app.configs.database import get_session_stub
 user_router = APIRouter()
 
 @user_router.post('/create')
-def create(name, email, password, session = Depends(get_session_stub)):
+def create(name: str, email: str, password: str, money: int, session = Depends(get_session_stub)):
 
     user_repository = UserRepository(session)
     user_interactor = UserInteractor(user_repository)
 
-    user_interactor.create(name, email, password)
+    user_interactor.create(name, email, password, money)
+
+    return 200
+
+@user_router.get('/read')
+def read(name: str = None, session = Depends(get_session_stub)):
+    user_repository = UserRepository(session)
+    user_interactor = UserInteractor(user_repository)
+
+    result = user_interactor.read(name)
+
+    return result
+
+@user_router.put('/update')
+def update(name: str, money: int, session = Depends(get_session_stub)):
+    user_repository = UserRepository(session)
+    user_interactor = UserInteractor(user_repository)
+
+    user_interactor.update(name, money)
+
+    return 200
+
+@user_router.delete('/delete')
+def delete(name: str, session = Depends(get_session_stub)):
+    user_repository = UserRepository(session)
+    user_interactor = UserInteractor(user_repository)
+
+    user_interactor.delete(name)
 
     return 200
