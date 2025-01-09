ee512d519a3e918564d4a7a503bcaa48aeb8ec07
0xwebmilk <0xwebmilk@gmail.com>
2025-01-09 05:08:40 +0300

    Added CRUD

--- app/interactors/user_interactor.py
+++ app/interactors/user_interactor.py
@@ -2,5 +2,14 @@ class UserInteractor:
     def __init__(self, repository):
         self.repository = repository
 
-    def create(self, name, email, password):
-        return self.repository.create(name, email, password)
+    def create(self, name, email, password, money):
+        return self.repository.create(name, email, password, money)
+
+    def read(self, name):
+        return self.repository.read(name)
+
+    def update(self, name, money):
+        return self.repository.update(name, money)
+
+    def delete(self, name):
+        return self.repository.delete(name)
